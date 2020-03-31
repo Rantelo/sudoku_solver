67131eb10f725a9699a956f680a95cd9919a9b88
Roberto Antelo <roberto_antelo@hotmail.com>
2020-03-31 15:01:36 -0600

    Publish to gh pages

---
+++ docs/precache-manifest.fec9e473496073050ebb913efb8df0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50eb1f7e8bb13262f241d84fa5b66986",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86fe0ac8daa512c7c1aa",
+    "url": "/static/css/main.400d41a1.chunk.css"
+  },
+  {
+    "revision": "7a4872993b1692b9d47f",
+    "url": "/static/js/2.71b3a233.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.71b3a233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fe0ac8daa512c7c1aa",
+    "url": "/static/js/main.c54a9055.chunk.js"
+  },
+  {
+    "revision": "65d16b48a53c5418f41d",
+    "url": "/static/js/runtime-main.87ea8d36.js"
+  }
+]);
